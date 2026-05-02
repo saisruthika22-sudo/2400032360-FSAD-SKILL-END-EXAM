1a5df442a5593907c0e203eb0011755b75d30361
saisruthika22-sudo <saisruthika22@gmail.com>
2026-05-02 15:32:15 +0530

    Update InvoiceServiceImpl.java

--- src/main/java/com/klef/fsad/exam/service/imp/InvoiceServiceImpl.java
+++ src/main/java/com/klef/fsad/exam/service/imp/InvoiceServiceImpl.java
@@ -1,5 +1,32 @@
-package com.klef.fsad.exam.service.imp;
+package com.klef.fsad.exam.service.impl;
 
-public class InvoiceServiceImpl {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.klef.fsad.exam.model.Invoice;
+import com.klef.fsad.exam.repository.InvoiceRepository;
+import com.klef.fsad.exam.service.InvoiceService;
+
+@Service
+public class InvoiceServiceImpl implements InvoiceService {
+
+    @Autowired
+    private InvoiceRepository repository;
+
+    @Override
+    public Invoice addInvoice(Invoice invoice) {
+        return repository.save(invoice);
+    }
+
+    @Override
+    public List<Invoice> getAllInvoices() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Invoice getInvoiceById(Long id) {
+        return repository.findById(id).orElse(null);
+    }
 }
