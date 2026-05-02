f6cf72195fb97e82e736172516cec30476011ea8
saisruthika22-sudo <saisruthika22@gmail.com>
2026-05-02 15:30:36 +0530

    Update Invoice.java

--- src/main/java/com/klef/fsad/exam/model/Invoice.java
+++ src/main/java/com/klef/fsad/exam/model/Invoice.java
@@ -1,5 +1,58 @@
 package com.klef.fsad.exam.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Invoice {
 
+    @Id
+    private Long invoiceId;   // manually given
+
+    private String name;
+    private String date;
+    private String status;
+    private double amount;
+
+    public Invoice() {}
+
+    public Long getInvoiceId() {
+        return invoiceId;
+    }
+
+    public void setInvoiceId(Long invoiceId) {
+        this.invoiceId = invoiceId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
 }
