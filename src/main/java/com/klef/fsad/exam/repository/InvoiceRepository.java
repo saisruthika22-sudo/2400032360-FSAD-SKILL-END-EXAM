8b762e17bcb8f6062e0e175ac0a0fa9b8602ff1a
saisruthika22-sudo <saisruthika22@gmail.com>
2026-05-02 15:31:05 +0530

    Update InvoiceRepository.java

--- src/main/java/com/klef/fsad/exam/repository/InvoiceRepository.java
+++ src/main/java/com/klef/fsad/exam/repository/InvoiceRepository.java
@@ -1,5 +1,8 @@
 package com.klef.fsad.exam.repository;
 
-public class InvoiceRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.klef.fsad.exam.model.Invoice;
+
+public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
 
 }
